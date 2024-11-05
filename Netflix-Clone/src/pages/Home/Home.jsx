912cf82e5d24fd12e85b780c84c664c558c8d8de
account2024 <kebekeneni90@gmail.com>
2024-11-04 18:24:31 -0600

    Rows added

--- Netflix-Clone/src/pages/Home/Home.jsx
+++ Netflix-Clone/src/pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import "./Home.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import Banner from "../../components/Banner/Banner";
+import RowList from "../../components/Rows/RowList/RowList";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
   );
